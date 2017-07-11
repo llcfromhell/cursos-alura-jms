9aa8b237237b31116f0676c39ec8f3f5c8eae0ec
Lourenço Lima da Cunha <lourenco.cunha@POA.E-CORE.COM>
2017-07-11 12:45:15 -0300

    aula 03

--- jms/src/main/java/br/com/alura/jms/TesteConsumidor.java
+++ jms/src/main/java/br/com/alura/jms/TesteConsumidor.java
@@ -5,9 +5,12 @@ import java.util.Scanner;
 import javax.jms.Connection;
 import javax.jms.ConnectionFactory;
 import javax.jms.Destination;
+import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.MessageConsumer;
+import javax.jms.MessageListener;
 import javax.jms.Session;
+import javax.jms.TextMessage;
 import javax.naming.InitialContext;
 
 public class TesteConsumidor {
@@ -21,14 +24,29 @@ public class TesteConsumidor {
         Connection connection = factory.createConnection();
         connection.start();
 
-        new Scanner(System.in).nextLine(); //parar o programa para testar a conexao
+        
 
         Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
         Destination fila = (Destination) context.lookup("financeiro");
         MessageConsumer consumer = session.createConsumer(fila);
 
-        Message message = consumer.receive();
-        System.out.println("Recebendo msg: "+ message);
+        consumer.setMessageListener(new MessageListener(){
+
+            @Override
+            public void onMessage(Message message){
+            	TextMessage textMessage  = (TextMessage)message;
+                try{
+                    System.out.println(textMessage.getText());
+                } catch(JMSException e){
+                    e.printStackTrace();
+                }
+            }
+
+        });
+        
+        //Message message = consumer.receive();
+        
+        new Scanner(System.in).nextLine(); //parar o programa para testar a conexao
 
         session.close();
         
